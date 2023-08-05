606e5dd31ee06a778c4da98d898e8f396be74679
Aaron Renoir <aaron@catzo.com>
2023-08-05 18:52:19 +0100

    update plugin

--- _plugins/view_image_tag.rb
+++ _plugins/view_image_tag.rb
@@ -9,9 +9,33 @@ module Jekyll
     def render(context)
       @context = context
       site = context.registers[:site]
-      relative_path = Liquid::Template.parse(@relative_path).render(context)
-      image_url = "#{relative_path}/assets/images/#{@image_path}"
+      image_url = compute_relative_url("/assets/images/#{@image_path}")
       "<a class='view-image' href='#{image_url}' onclick='return viewImage(event);'>📷</a>"
+    end
+
+    private
+
+    def compute_relative_url(input)
+      return input if Addressable::URI.parse(input.to_s).absolute?
+
+      parts = [sanitized_baseurl, input]
+      Addressable::URI.parse(
+        parts.map! { |part| ensure_leading_slash(part.to_s) }.join
+      ).normalize.to_s
+    end
+
+    def sanitized_baseurl
+      site = @context.registers[:site]
+      baseurl = site.config["baseurl"]
+      return "" if baseurl.nil?
+
+      baseurl.to_s.chomp("/")
+    end
+
+    def ensure_leading_slash(input)
+      return input if input.nil? || input.empty? || input.start_with?("/")
+
+      "/#{input}"
     end
   end
 end
